f1e4722af850f5caa3022823f94b1aa48c3f33be
DJ <dennis.ji@sap.com>
2016-04-15 16:32:03 +0800

    update code

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-hexo generate -d
+hexo generate
 cp -R public/* .deploy/jiji262.github.io
-cd .deploy/jiji262.githucd ../b.io
+cd .deploy/jiji262.github.io
 git add .
 git commit -m “update”
 git push origin master
+pause
